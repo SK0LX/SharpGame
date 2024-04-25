a12f88ba6a2d0ec1ec757c30ea997c00acfd5896
SKOLX <d.butakov2005@gmail.com>
2024-04-25 23:44:04 +0500

    Почти доделан файт

--- DarkPortal/Assets/PlayerController.cs
+++ DarkPortal/Assets/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     public Animator Animator;
-    private bool facingRight = true;
     private float moveInput;
     private int hp;
     private int dexterity;
@@ -28,18 +27,14 @@ public class PlayerController : MonoBehaviour
 
     private void CallEvent()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            StartCoroutine(Attack());
-        }
-
         if (Input.GetKeyDown(KeyCode.L))
         {
             var health = gameObject.GetComponent<Health>();
             health.TakeHit(10);
         }
     }
-
+    
+    
     private IEnumerator movementPlayer()
     {
         var horizontalMove = Input.GetAxis("Horizontal") * speed;
@@ -64,54 +59,5 @@ public class PlayerController : MonoBehaviour
             Animator.SetFloat("HorizontalMove", 0);
             yield return new WaitForSeconds(1f);
         }
-    }
-
-    public void OnButtonClick()
-    {
-        StartCoroutine(Attack());
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Fight"))
-        {
-            speed = 0;
-        }
-    }
-    
-    void FlipForFight()
-    {
-        facingRight = !facingRight;
-        var Scalar = transform.localScale;
-        Scalar.x *= -1;
-        transform.localScale = Scalar;
-    }
-
-    IEnumerator Attack()
-    {
-        var geolocationNow = transform.position.x;
-        var moveSpeed = 50f; // Скорость движения
-        
-        Animator.SetTrigger("runForAttack1");
-        while (transform.position.x < geolocationNow + 70)
-        {
-            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-            yield return null;
-        }
-
-        Animator.SetTrigger("attack1");
-        yield return new WaitForSeconds(1f);
-        FlipForFight();
-        
-        Animator.SetTrigger("runForAttack1");
-        
-        while (transform.position.x > geolocationNow)
-        {
-            transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-            yield return null;
-        }
-        
-        FlipForFight();
-        Animator.SetTrigger("default");
     }
 }
