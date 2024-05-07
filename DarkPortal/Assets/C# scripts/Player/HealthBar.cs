1a2fef8ec69d3b7f581fb47c64ea587cdcd5dae7
SKOLX <d.butakov2005@gmail.com>
2024-05-07 12:12:37 +0500

    Добавлены интерфейс победы и поражения

--- DarkPortal/Assets/C# scripts/Player/HealthBar.cs
+++ DarkPortal/Assets/C# scripts/Player/HealthBar.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Image bar;
     public Image barForDefault;
     public float fill;
     void Start()
@@ -17,7 +16,6 @@ public class HealthBar : MonoBehaviour
     
     void Update()
     {
-        bar.fillAmount = fill;
         barForDefault.fillAmount = fill;
     }
 }
